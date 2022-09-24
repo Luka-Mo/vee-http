e7ff2d64f2e95c679565669f96b48a0e142ff145
Luka M <lukam@nym.hush.com>
2022-09-24 07:18:00 +0200

    chore: add request function chore: update d.ts files

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import type {App} from "vue";
 import {initHandler} from "./lib/core/xhr-handler";
-import {del, get, head, options, patch, post, put} from "./lib/core/methods";
+import {request, del, get, head, options, patch, post, put} from "./lib/core/methods";
 import {VHttpInterceptor} from "./lib/models/v-http-models";
 
 let _Vue: App;
 
 const vHttp = {
+  request,
   get,
   post,
   put,
